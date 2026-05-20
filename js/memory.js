b39b75621f953dc2361dbc485b582b7d797ee1cf
LeninK8 <joslias1003@gmail.com>
2026-05-20 16:37:30 -0500

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,1 +1,29 @@
+export const KIRA_MEMORY = {
+    memory: [],
+    maxSize: 60,
 
+    init: function() {
+        this.memory = JSON.parse(localStorage.getItem('kira_memory')) || [];
+    },
+
+    addMessage: function(role, content) {
+        this.memory.push({
+            role: role,
+            content: content,
+            time: new Date().toLocaleTimeString()
+        });
+
+        if (this.memory.length > this.maxSize) this.memory.shift();
+        localStorage.setItem('kira_memory', JSON.stringify(this.memory));
+    },
+
+    getHistory: function() {
+        return this.memory;
+    },
+
+    clear: function() {
+        this.memory = [];
+        localStorage.removeItem('kira_memory');
+        console.log("%c[K|RΛ] Memoria borrada completamente.", "color:#ff00ff");
+    }
+};
